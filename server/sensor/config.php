714a36e3ad10982b5d942e35763c5012d82f8e4b
Kenichi Ohwada <ohwada@ohwada-no-MacBook-Air.local>
2014-11-22 21:59:51 +0900

    FirstVersion

--- server/sensor/config.php
+++ server/sensor/config.php
@@ -5,9 +5,11 @@
  * 2014-08-20
  */
 
+// please rewrite your name & password
 	define( "DB_USER", "yourName" );
 	define( "DB_PASS", "YourPassword" );
 
+// tilte
 	define( "TITLE", "FabLab Kannai Sensor Project" );
 
 ?>
